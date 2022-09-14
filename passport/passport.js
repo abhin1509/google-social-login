28f3673d3750052917a5fe256de3d617fd753e32
Abhinav Kumar <abhin1509@gmail.com>
2022-09-14 23:00:50 +0530

    saving profile in db

--- passport/passport.js
+++ passport/passport.js
@@ -1,5 +1,7 @@
 const passport = require("passport");
 require("dotenv").config();
+const User = require("../model/user");
+
 var GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 passport.use(
@@ -10,7 +12,28 @@ passport.use(
       callbackURL: "http://localhost:4000/auth/google/callback",
     },
     (accessToken, refreshToken, profile, next) => {
-      console.log("my profile :: ", profile);
+      console.log("my profile :: ", profile._json.email);
+
+      User.findOne({ email: profile._json.email }).then((user) => {
+        //user in db
+        if (user) {
+          console.log("user already in db", user);
+          next(null, user);
+          // cookie token
+        } else {
+          User.create({
+            name: profile.displayName,
+            googleId: profile.id,
+            email: profile._json.email,
+          })
+            .then((user) => {
+              console.log("new user ", user);
+              next(null, user);
+              //cookie token
+            })
+            .catch((err) => console.log(err));
+        }
+      });
       next();
     }
   )
